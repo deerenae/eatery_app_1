677d426e9ff3e34bf78129bd8911a7b3dc5b5af8
github username <github email address>
2020-06-30 01:39:23 -0600

    added frontend

--- eatery_app_1/settings.py
+++ eatery_app_1/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'eatery_backend',
     'rest_framework', 
+    'frontend'
     
 ]
 
---
+++ frontend/src/components/restaurants/Dashboard.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+import Restaurants from './Restaurants';
+
+export default function Dashboard() {
+    return (
+        <Fragment>
+        
+            <Restaurants />
+
+        </Fragment>
+    )         
+}
+
